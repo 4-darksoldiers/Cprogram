1dd4663e630011b05cddcad57d5e511d90e39615
arthurprotype <sjc@sjc-PC>
2020-06-09 16:36:52 +0800

    change_genbank.h

--- genbank.h
+++ genbank.h
@@ -7,10 +7,10 @@
 #include "cds.h"
 
 //private function
-void initial_cds(CDS cds);              //初始化结构体
+void initial_cds(CDS *cds);              //初始化结构体
 void line_div(char *str, char *line[]); //主要用于提取cds.gene，把含有引号的字符串划分，提取引号内的字符串
 
 //public function
-void get_cds(CDS cds, FILE *file); //依次提取结构体中的各内容
+void get_cds(CDS *cds, FILE *file); //依次提取结构体中的各内容
 
 #endif
